11b64d7942ac53d8e30d37983af2425b5c893814
nkkmkk <nikitmkk@gmail.com>
2025-05-10 18:16:05 +0200

    better readability

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ class Person:
         Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
+def create_person_list(people: list) -> list[Person]:
     peoples_list = [
         Person(person["name"], person["age"])
         for person in people
@@ -16,7 +16,11 @@ def create_person_list(people: list) -> list:
     for person in people:
         person_instance = Person.people.get(person["name"])
         if "wife" in person and person["wife"] is not None:
-            person_instance.wife = Person.people.get(person["wife"])
+            person_instance.wife = Person.people.get(
+                person["wife"]
+            )
         if "husband" in person and person["husband"] is not None:
-            person_instance.husband = Person.people.get(person["husband"])
+            person_instance.husband = Person.people.get(
+                person["husband"]
+            )
     return peoples_list
